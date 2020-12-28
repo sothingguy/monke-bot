252e48e74541f314d26b499ba41f76e1b912d725
Sothingguy <sothingguy@gmail.com>
2020-12-28 21:58:06 +1300

    changes were made

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log("Learner bot is online!");
+	client.user.setActivity("With my robot cock"); 
 });
 
 client.on('message', message => {
@@ -15,11 +16,9 @@ client.on('message', message => {
 		message.channel.send('pong.');
 	} else if (message.content === `${prefix}avatar`) {
 		message.channel.send(message.author.displayAvatarURL());
-	} else if (message.content === `${prefix}ape`) { // make monket noises
+	} else if (message.content === `${prefix}ape`) { // make monkey noises
 		// Checking if the message author is in a voice channel.
 		if (!message.member.voice.channel) return message.reply("You must be in a voice channel.");
-		// Checking if the bot is in a voice channel.
-		if (message.guild.me.voice.channel) return message.reply("I'm already playing.");
 
 		// Joining the channel and creating a VoiceConnection.
 		message.member.voice.channel.join().then(VoiceConnection => {
@@ -31,15 +30,8 @@ client.on('message', message => {
 
 			// play actual sound
 			VoiceConnection.play(file).on("finish", () => VoiceConnection.disconnect());
-			message.reply("Playing...");
 		}).catch(e => console.log(e))
-	} else if (message.content === `${prefix}Foff`) { // leave voice channel
-		// cheek if in voice channel
-		if (message.guild.me.voice.channel) return message.reply("I'm not in a voice channel.");
-		
-		// leave voice chanel if in one
-		VoiceConnection.disconnect()
-	};
+	}
 });
 
 client.login(token);
