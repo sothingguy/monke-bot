9fd9daa0e61bc368619a4d49d5b179c2e3f62a33
Sothingguy <sothingguy@gmail.com>
2020-12-30 20:58:20 +1300

    gc command added

---
+++ commands/gc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'gc',
+	description: 'plays good cunt compilation',
+	execute(message, args) {
+        const fs = require('fs');
+		// Checking if the message author is in a voice channel.
+		if (!message.member.voice.channel) return message.reply("You must be in a voice channel.");
+
+		// Joining the channel and creating a VoiceConnection.
+		message.member.voice.channel.join().then(VoiceConnection => {
+
+			// play actual audio
+			VoiceConnection.play("./sounds/gc.mp3");
+		})
+	},
+};
