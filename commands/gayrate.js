6f0521bf35e331ee4248d01e4f78071a5012f2a5
Sothingguy <sothingguy@gmail.com>
2020-12-30 18:05:38 +1300

    duh

--- commands/gayrate.js
+++ commands/gayrate.js
@@ -5,7 +5,11 @@ module.exports = {
         if (!args.length) {
 			rated = message.member.user.tag;
 		} else{
-            rated = message.content.slice(8).trim();
+			rated = message.content.slice(8).trim();
+			if (rated == "daniel") {
+				message.reply(rated + " is " + "110" + "% gay! :rainbow_flag:");
+				return
+			}
         };
         message.reply(rated + " is " + Math.floor(Math.random() * 100) + "% gay! :rainbow_flag:");
 	},
