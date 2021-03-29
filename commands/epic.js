b9e15ee4a7cf3d358730190c69925e9b6bf928dd
Sothingguy <sothingguy@gmail.com>
2021-03-29 23:06:19 +1300

    adding a random video generator

---
+++ commands/epic.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'epic',
+	description: 'sends link to random yoututbe video with almost no views',
+	execute(message, args) {
+        const fs = require('fs');
+		fs.readFile('commands/randomVid.json', (err, data) => {
+			if (err) throw err;
+			let link = JSON.parse(data);
+            link = link['link']
+			message.reply('click here for funny --->' + link);
+		});
+	},
+};
